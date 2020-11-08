48dd4787d7cd07886c60ee209c978d924ed9a5a2
Anton Bracke <anton@ju60.de>
2020-11-08 14:48:23 +0100

    fix path

--- packages/backend/src/services/users/users.model.ts
+++ packages/backend/src/services/users/users.model.ts
@@ -1,6 +1,6 @@
 import NeDB from 'nedb';
 import path from 'path';
-import { Application } from '../declarations';
+import { Application } from '../../declarations';
 
 export default (app: Application): NeDB<any> => {
   const dbPath = app.get('nedb');
