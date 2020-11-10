d8974fe756338d8dfaa8cec9c32a56d20f7a5660
Julian <julian.hahn@student.fh-kiel.de>
2020-11-10 17:33:01 +0100

    created two new components for input/output

---
+++ packages/app/src/components/OutputList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'OutputList',
+  data() {
+    return {
+    };
+  },
+});
+</script>
