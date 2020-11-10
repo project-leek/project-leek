19f770e9b887807df6700d4447adbf51c5863ab8
Julian <julian.hahn@student.fh-kiel.de>
2020-11-10 17:31:29 +0100

    the main javascript file should only mount the vue app

--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,14 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import './index.css';
-
-import test from './test';
-
-function printIt(str: number) {
-  // eslint-disable-next-line no-console
-  console.log(str);
-}
-
-printIt(test.text);
 
 createApp(App).mount('#app');
