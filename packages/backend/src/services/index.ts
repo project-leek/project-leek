5d3d9f95b6c61998079aede1f6754c984ffb1329
Nick Loewecke <konten@loewecke.de>
2020-11-13 15:46:36 +0100

    Created Backend-Service for Nicks Pet

--- packages/backend/src/services/index.ts
+++ packages/backend/src/services/index.ts
@@ -1,7 +1,11 @@
 import { Application } from '../declarations';
 import users from './users/users.service';
+import nicksPet from './nicksPet/nicksPet.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
+  app.configure(nicksPet);
 }
+
+
