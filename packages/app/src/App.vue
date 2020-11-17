7a7effef9970bc9c14172bce4380c1a194f758bf
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2020-11-17 22:32:10 +0100

    I dont know how to stash so here it is

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
 import HelloWorld from './components/HelloWorld.vue';
-
 export default defineComponent({
   name: 'App',
-
   components: {
     HelloWorld,
   },
--- packages/app/src/components/HelloWorld.vue
+++ packages/app/src/components/HelloWorld.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
-    Hello Rudolfo
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+    <p>
+      Edit <code>components/HelloWorld.vue</code> to test hot module
+      replacement.
+    </p>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue';
-
 import feathers from '@/lib/feathers';
-
-export default ({ 
+export default defineComponent({
   name: 'HelloWorld',
+  props: {
+    msg: {
+      type: String,
+      default: 'Hexe',
+    },
+  },
   setup() {
     const count = ref(0);
     const users = ref([]);
-
     onMounted(async () => {
       users.value = await feathers.service('users').find();
     });
+    return {
+      count,
+    };
   },
 });
 </script>
