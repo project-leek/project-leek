6490337a25d4546aee79560c574a8d0a2cc892fb
Julian Hahn <julianhahn01@gmail.com>
2020-11-23 00:16:33 +0100

    display the database as output list

--- packages/app/src/components/OutputList.vue
+++ packages/app/src/components/OutputList.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="w-full h-full object-cover card bg-white shadow-2xl max-w-2xl">
+  <div
+    class="w-full h-full object-cover card bg-white shadow-2xl max-w-2xl overflow-auto"
+  >
     <entry v-for="pet in pets" :key="pet.name" :pet="pet"></entry>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
+import { Pet } from '@project-leek/commons';
+import { Paginated } from '@feathersjs/feathers';
+import feathers from '../lib/feathers';
 import entry from './Entry.vue';
 
 export default defineComponent({
@@ -13,25 +18,23 @@ export default defineComponent({
   components: {
     entry,
   },
-  data: () => ({
-    pets: [
-      {
-        name: 'Doggo',
-        breed: 'Bloodhound',
-        // important! year and month are zero indexed!
-        dateOfBirth: new Date(2002, 11, 24),
-      },
-      {
-        name: 'Hasso',
-        breed: 'Mops',
-        dateOfBirth: new Date(2012, 3, 9),
-      },
-      {
-        name: 'Good Boy',
-        breed: 'Border Collee',
-        dateOfBirth: new Date(25, 7, 2020),
-      },
-    ],
-  }),
+  setup() {
+    const pets = ref<Pet[]>([]);
+
+    onMounted(async () => {
+      const response = (await feathers.service('pets').find()) as Paginated<
+        Pet
+      >;
+      pets.value = response.data;
+    });
+
+    feathers.service('pets').on('created', (newPet) => {
+      pets.value.push(newPet);
+    });
+
+    return {
+      pets,
+    };
+  },
 });
 </script>
