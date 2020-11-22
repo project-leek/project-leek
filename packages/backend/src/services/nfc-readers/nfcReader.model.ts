f768d652bcfb4d58b1c09441ec68d7af780fe313
Nick Loewecke <konten@loewecke.de>
2020-11-22 21:26:09 +0100

    Created Service and Model for NFC-Reader

---
+++ packages/backend/src/services/nfc-readers/nfcReader.model.ts
@@ -0,0 +1,14 @@
+import NeDB from 'nedb';
+import path from 'path';
+import { Application } from '../../declarations';
+
+export default (app: Application): NeDB<any> => {
+  const dbPath = app.get('nedb');
+  const Model = new NeDB({
+    filename: path.join(dbPath, 'nfcReaders.db'),
+    autoload: true,
+    inMemoryOnly: process.env.NODE_ENV === 'test',
+  });
+
+  return Model;
+};
---
+++ packages/backend/src/services/nfc-readers/nfcReader.service.ts
@@ -0,0 +1,32 @@
+import { ServiceAddons } from '@feathersjs/feathers';
+import { Service, NedbServiceOptions } from 'feathers-nedb';
+import { NFCReader } from '@project-leek/commons';
+import { Application } from '../../declarations';
+import createModel from './nfcReader.model';
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    nfcReaders: NFCReaderService & ServiceAddons<NFCReader>;
+  }
+}
+
+class NFCReaderService extends Service<NFCReader> {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  constructor(options: Partial<NedbServiceOptions>, app: Application) {
+    super(options);
+  }
+}
+
+export default (app: Application): void => {
+  const options = {
+    Model: createModel(app),
+    paginate: app.get('paginate'),
+  };
+
+  // Initialize our service with any options it requires
+  app.use('nfcReaders', new NFCReaderService(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('nfcReaders');
+};
