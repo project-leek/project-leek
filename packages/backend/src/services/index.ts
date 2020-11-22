5a2394770e8ea17ee61e3bff013c9cf6a833a0c4
Nick Loewecke <konten@loewecke.de>
2020-11-22 21:28:44 +0100

    Added Service to index.ts

--- packages/backend/src/services/index.ts
+++ packages/backend/src/services/index.ts
@@ -1,7 +1,9 @@
 import { Application } from '../declarations';
 import users from './users/users.service';
+import nfcReaders from './nfc-readers/nfcReader.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default (app: Application): void => {
   app.configure(users);
+  app.configure(nfcReaders);
 };
