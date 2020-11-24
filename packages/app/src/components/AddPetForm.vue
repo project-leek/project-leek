6c2b85a2e46752c8a6fa444317cfef3b08658f7a
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2020-11-24 13:21:51 +0100

    added form and posting to feathers service

--- packages/app/src/components/AddPetForm.vue
+++ packages/app/src/components/AddPetForm.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>AddPetForm</div>
+  <div>
+    PET FORM:
+    <br />
+    <input v-model="name" placeholder="name">
+    <p>name is: {{name}}</p>
+    <input v-model="type" placeholder="type">
+    <p>name is: {{type}}</p>
+    <button @click="createPet">add Pet</button>
+
+  </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import feathers from '../lib/feathers';
+import { Rudolfo } from '@project-leek/commons';
+
+export default defineComponent({
   name: 'AddPetForm',
-};
+
+  setup() {
+    const name = ref("");
+    const type = ref("");
+
+    const createPet = async () => {
+      const pet: Rudolfo = {
+        name: name,
+        type: type,
+      };
+
+      await feathers.service('rudolfo').create(pet);
+      name.value = "";
+      type.value = "";
+    };
+
+    return {
+      createPet,
+      name,
+      type,
+    };
+  },
+});
 </script>
