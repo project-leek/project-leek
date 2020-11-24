f4f13fa8447845937317ea66a0adcc29f7c0ed1a
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2020-11-24 13:22:11 +0100

    added feathers service and refactored code

--- packages/app/src/components/HelloWorld.vue
+++ packages/app/src/components/HelloWorld.vue
@@ -1,49 +1,19 @@
 <template>
   <div>
-    <button @click="buttonClick">count is: {{ count }}</button>
-    <PetList :petlist="rudolfoList" />
-
+    <AddPetForm />
+    <br/>
+    <br/>
+    <PetList/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
-import feathers from '@/lib/feathers';
-import { Rudolfo, User } from '@project-leek/commons';
+import { defineComponent } from 'vue';
 import PetList from './PetList.vue';
+import AddPetForm from './AddPetForm.vue';
 
 export default defineComponent({
   name: 'HelloWorld',
-  components: { PetList },
-
-  setup() {
-    const count = ref(0);
-    const users = ref<User[]>([]);
-
-    onMounted(async () => {
-      // users.value = await feathers.service('users').find();
-    });
-
-    const rudolfoList = ref<Rudolfo[]>([]);
-
-    function buttonClick() {
-      count.value += 1;
-      rudolfoList.value = [];
-      for (let index = 0; index < count.value; index += 1) {
-        const pet: Rudolfo = {
-          name: "rudolfo",
-          type: "behindert",
-        };
-        rudolfoList.value.push(pet);
-      }
-    }
-
-    return {
-      count,
-      users,
-      buttonClick,
-      rudolfoList,
-    };
-  },
+  components: { PetList, AddPetForm },
 });
 </script>
--- packages/app/src/components/PetList.vue
+++ packages/app/src/components/PetList.vue
@@ -1,15 +1,35 @@
 <template>
-  <div> HERE IS A PETLIST <br/>
-  <div v-for="pet in petlist">
-    {{pet.name}}
-  </div>
-
+  <div>
+    HERE IS A PETLIST <br />
+    <div v-for="rudolfo in rudolfos">
+      {{ rudolfo.name }}
+      {{ rudolfo.type }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import { defineComponent } from "vue";
+import feathers from '@/lib/feathers';
+import { Rudolfo } from '@project-leek/commons';
+
+export default defineComponent{
     name: "PetList",
-    props:["petlist"],
+
+  setup() {
+
+    const rudolfos = ref<Rudolfo[]>([]);
+    onMounted(async () => {
+      rudolfos.value = await feathers.service('rudolfo').find();
+    });
+
+
+    return {
+      count,
+      users,
+      buttonClick,
+      rudolfos,
+    };
+  },
 };
 </script>
