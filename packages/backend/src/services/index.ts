a84defc49f7ce7c29fbdee2fb843b0af329cc024
Anbraten <anton@ju60.de>
2020-11-24 19:29:15 +0100

    Apply suggestions from code review
    Co-authored-by: schnickalodeon <45515005+schnickalodeon@users.noreply.github.com>

--- packages/backend/src/services/index.ts
+++ packages/backend/src/services/index.ts
@@ -1,7 +1,7 @@
 import { Application } from '../declarations';
 import antonsPets from './antons-pets/antons-pets.service';
 import users from './users/users.service';
-import JansPets from './jans-pets/jans-pets.services';
+import jansPets from './jans-pets/jans-pets.services';
 import pets from './pets/pets.service';
 import nfcReaders from './nfc-readers/nfc-reader.service';
 
