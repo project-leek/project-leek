41483a3c4bde8ccc47719d9553226a0f361d728d
Anbraten <anton@ju60.de>
2020-11-24 21:15:28 +0100

    Update packages/backend/src/services/nfc-tags/nfc-tag.service.ts

--- packages/backend/src/services/nfc-tags/nfc-tag.service.ts
+++ packages/backend/src/services/nfc-tags/nfc-tag.service.ts
@@ -21,6 +21,7 @@ class NFCTagService extends Service<NFCTag> {
 export default (app: Application): void => {
   const options = {
     Model: createModel(app),
+    paginate: app.get('paginate'),
   };
 
   // Initialize our service with any options it requires
