31e2302cae7ea440bfff9aa864a4ff56e6b0d890
Julian Hahn <julianhahn01@gmail.com>
2020-11-24 20:56:15 +0100

    add nfc tag model and service

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -1,9 +1,9 @@
 import User from './user.class';
 import NFCReader from './nfc-reader.class';
-
+import NFCTag from './nfc-tag.class';
 import NicksPet from './nicks-pet.class';
 import JansPet from './jans-pet.class';
 import AntonsPet from './antons-pet.class';
 import Pet from './pet.class';
 
-export { User, NFCReader, AntonsPet, Pet, JansPet, NicksPet };
+export { User, NFCReader, NFCTag, AntonsPet, Pet, JansPet, NicksPet };
