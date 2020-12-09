d1dac04aa21f988333940cc27c168fbcff1b35fb
Jan Eberlein <eberlein.jan@aol.de>
2020-12-09 13:12:51 +0100

    - moves seed data into seed function - fixes json read error by removing json file

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,13 +1,27 @@
 import { Server } from 'http';
-import { NFCTag } from '@project-leek/commons';
 
 import app from './app';
 import logger from './logger';
 import webserver from './webserver';
-import seedNFCTags from './seed_data/seed_nfc_tags.json';
 
 const NODE_ENV = process.env.NODE_ENV || 'production';
 let server: Server;
+
+async function seed() {
+  await app.service('nfc-readers').create({
+    _id: '', // use a fixed id to prevent duplicates
+    owner: 'aE6IRWdETCdeOGjo',
+  });
+  await app
+    .service('nfc-tags')
+    .create({ nfcId: '2589851589', spotifyTrackUri: 'spotify:track:2ej1A2Ze6P2EOW7KfIosZR' });
+  await app
+    .service('nfc-tags')
+    .create({ nfcId: '2589689541', spotifyTrackUri: 'spotify:track:4uLU6hMCjMI75M1A2tKUQC' });
+  await app
+    .service('nfc-tags')
+    .create({ nfcId: '2589928949', spotifyTrackUri: 'spotify:track:6tYlLMni6GwUksie3N6IPA' });
+}
 
 function start() {
   logger.info('Application (%s v%s) starting ...', NODE_ENV, 'unkown');
@@ -22,11 +36,9 @@ function start() {
 
   app.setup(server);
 
-  const seededTags: Partial<NFCTag>[] = seedNFCTags;
-  app
-    .service('nfc-tags')
-    .create(seededTags)
-    .catch(() => {});
+  seed().catch((error: Error) => {
+    console.log('Seeding failed', error.message);
+  });
 }
 
 async function stop(): Promise<void> {
