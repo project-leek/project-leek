9d338b2b67663509cc27ee1ee81cfd8f79271cc3
Anton Bracke <anton@ju60.de>
2020-12-09 12:49:01 +0100

    :lipstick: improve code-style

--- packages/backend/src/services/nfc-readers/hooks/index.ts
+++ packages/backend/src/services/nfc-readers/hooks/index.ts
@@ -1,33 +1,8 @@
 import playSpotify from './play-spotify';
 
 export default {
-  before: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
-  },
-
   after: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
     update: [playSpotify],
     patch: [playSpotify],
-    remove: [],
-  },
-
-  error: {
-    all: [],
-    find: [],
-    get: [],
-    create: [],
-    update: [],
-    patch: [],
-    remove: [],
   },
 };
