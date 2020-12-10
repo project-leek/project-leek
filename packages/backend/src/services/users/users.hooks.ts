270ab873d0c4f9c070cc3143e93ebb245c29f137
Anton Bracke <anton@ju60.de>
2020-12-10 11:27:49 +0100

    :bug: remove old forgotten code

--- packages/backend/src/services/users/users.hooks.ts
+++ packages/backend/src/services/users/users.hooks.ts
@@ -1,27 +1,20 @@
 import * as feathersAuthentication from '@feathersjs/authentication';
-import * as local from '@feathersjs/authentication-local';
-// Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = feathersAuthentication.hooks;
-const { hashPassword, protect } = local.hooks;
 
 export default {
   before: {
     all: [],
     find: [authenticate('jwt')],
     get: [authenticate('jwt')],
-    create: [hashPassword('password')],
-    update: [hashPassword('password'), authenticate('jwt')],
-    patch: [hashPassword('password'), authenticate('jwt')],
+    create: [],
+    update: [authenticate('jwt')],
+    patch: [authenticate('jwt')],
     remove: [authenticate('jwt')],
   },
 
   after: {
-    all: [
-      // Make sure the password field is never sent to the client
-      // Always must be the last hook
-      protect('password'),
-    ],
+    all: [],
     find: [],
     get: [],
     create: [],
