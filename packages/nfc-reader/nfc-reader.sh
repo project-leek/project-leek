143d3ee05422e2bd219a74bf5dab81a6860a842d
Anton Bracke <anton@ju60.de>
2020-12-10 11:28:38 +0100

    update helper scripts

--- packages/nfc-reader/nfc-reader.sh
+++ packages/nfc-reader/nfc-reader.sh
@@ -1,10 +1,12 @@
 #! /bin/sh
 
-docker run --rm \
-  --name ilse \
-  -e NFC_READER_ID=WyQUw7wYOVLPbPtM \
+docker pull anbraten/project-leek-nfc-reader:latest
+
+docker run -ti \
+  --name nfc-reader \
+  -e NFC_READER_ID=BU62UGHufeLvF763 \
   -e API_URL=http://192.168.0.182:3030 \
   -v /dev/input:/dev/input \
   -v /run/udev/data:/run/udev/data \
   --device /dev/input/by-id/usb-413d_2107-event-kbd \
-  anbraten/project-leek-nfc-reader:latest
+  anbraten/project-leek-nfc-reader
