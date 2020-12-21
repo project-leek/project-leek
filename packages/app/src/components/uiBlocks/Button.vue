23a8ff73c85f1293ae8ca0ba32db8cf726794971
Nick Loewecke <Nick.Loewecke@student.fh-kiel.de>
2020-12-21 14:31:45 +0100

    Removed WithLoading functionality (does not work at the moment)

--- packages/app/src/components/uiBlocks/Button.vue
+++ packages/app/src/components/uiBlocks/Button.vue
@@ -3,12 +3,12 @@
     class="bg-black cursor-pointer rounded-md text-center p-2 text-white shadow-lg"
     @click="doClick"
   >
-    {{ text }}
+    <span>{{ text }}</span>
   </button>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
@@ -25,33 +25,18 @@ export default defineComponent({
       type: Boolean,
       default: false,
     },
-    withLoading: {
-      type: Boolean,
-      default: false,
-    },
     disabled: {
       type: Boolean,
       default: false,
     },
   },
   emits: ['click'],
-  setup(props, ctx) {
+  setup(props) {
     const router = useRouter();
-    const isLoading = ref(false);
 
     const doClick = async () => {
       if (props.disabled) {
         return;
-      }
-
-      if (props.withLoading) {
-        isLoading.value = true;
-        // const asyncFunction = (ctx.$listeners.click as () => Promise<void>);
-        const asyncFunction = ctx.$listeners.click;
-        await asyncFunction();
-        isLoading.value = false;
-      } else {
-        ctx.emit('click');
       }
 
       if (props.to) {
@@ -64,9 +49,9 @@ export default defineComponent({
       }
     };
 
-    return { doClick };
+    return {
+      doClick,
+    };
   },
 });
 </script>
-
-<style scoped></style>
