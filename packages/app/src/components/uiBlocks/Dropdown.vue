821ccad298c8ee437c9478913da2a8f9dec04630
Jan Eberlein <eberlein.jan@aol.de>
2020-12-29 14:30:22 +0100

    - finalizes dropdown selector

--- packages/app/src/components/uiBlocks/Dropdown.vue
+++ packages/app/src/components/uiBlocks/Dropdown.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="bg-white rounded-2xl p-0.5 pr-1 pl-3 w-full flex flex-col border-yellow-400 border-2">
-    <div class="group flex w-full items-center" @click="dropdownExtended = !dropdownExtended">
+  <div
+    class="bg-white rounded-2xl p-0.5 px-1 w-full flex flex-col border-yellow-400 border-2 divide-y-2 divide-yellow-50"
+  >
+    <div class="flex w-full items-center pl-2" @click="dropdownExtended = !dropdownExtended">
       <span class="flex-1">{{ getHeaderText() }}</span>
       <img
         v-if="dropdownExtended"
@@ -13,9 +15,21 @@
         src="../../assets/icons/chevron-right.svg"
       />
     </div>
-    <div v-if="dropdownExtended">
-      <div v-for="(item, index) in itemData" :key="index" class="w-full" @click="itemClick(index)">
-        {{ item }} {{ index }}
+    <div v-if="dropdownExtended" class="divide-y divide-yellow-50 mr-6">
+      <div
+        v-for="(item, index) in selectableItemValues"
+        :key="index"
+        class="w-full pl-2"
+        @click="itemClick(item)"
+      >
+        {{ item }}
+      </div>
+      <div v-if="addItemOption" class="flex w-full items-center pt-0.5" @click="addItem()">
+        <img
+          class="flex-none bg-gradient-to-b from-yellow-500 to-yellow-300 rounded-full p-0.5 mr-2 w-5 h-5 justify-start"
+          src="../../assets/icons/plus.svg"
+        />
+        <span class="flex-1">{{ addItemText }}</span>
       </div>
     </div>
   </div>
@@ -23,16 +37,17 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'Dropdown',
   props: {
-    itemData: {
+    selectableItemValues: {
       type: Array,
       required: true,
     },
-    selectedItemIndex: {
-      type: Number,
+    modelValue: {
+      type: String,
       required: false,
       default: null,
     },
@@ -41,24 +56,47 @@ export default defineComponent({
       required: false,
       default: 'Please select an item',
     },
+    addItemOption: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    addItemText: {
+      type: String,
+      required: false,
+      default: 'Please add an element',
+    },
+    addItemRedirectTo: {
+      type: String,
+      required: false,
+      default: '',
+    },
   },
+  emits: ['update:modelValue'],
   setup(props) {
-    const currentlySelectedItemIndex = ref(props.selectedItemIndex);
+    const currentlySelectedItemValue = ref(props.modelValue);
     const dropdownExtended = ref(false);
+    const router = useRouter();
 
-    function itemClick(index: number) {
-      currentlySelectedItemIndex.value = index;
+    function itemClick(itemValue: number) {
+      currentlySelectedItemValue.value = itemValue;
       dropdownExtended.value = false;
+      this.$emit('update:modelValue', itemValue);
     }
 
     function getHeaderText() {
-      if (currentlySelectedItemIndex.value == null) {
+      if (currentlySelectedItemValue.value == null) {
         return props.placeholderText;
       }
-      return props.itemData[currentlySelectedItemIndex.value];
+      return currentlySelectedItemValue.value;
+    }
+
+    function addItem() {
+      router.push(props.addItemRedirectTo);
     }
 
     return {
+      addItem,
       itemClick,
       getHeaderText,
       dropdownExtended,
