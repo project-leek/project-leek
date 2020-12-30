c2ef31aaf71dbff4cf48e09ae7dcd8222629afa6
Julian Hahn <julianhahn01@gmail.com>
2020-12-31 00:50:12 +0100

    add a simple template which matches the sizes from the figma design

--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -1,35 +1,27 @@
 <template>
-  <div class="m-auto">
-    <pre>{{ user }}</pre>
-    <p>Suchefeld</p>
-    <Textfield v-model="text" class="mb-4" icon="search" placeholder="Titelsuche" />
-    <p>Textfield</p>
-    <Textfield v-model="text" class="mb-4" label="Title" placeholder="Titelsuche" />
-    <div
-      class="cursor-pointer bg-green-600 rounded-full text-center p-2 text-white"
-      @click="logout"
-    >
-      Logout
+  <div id="wrapper" class="mx-auto max-w-lg h-full w-full flex flex-col">
+    <!-- this is a basic template to fill the divs with content. The divs already match roughly the sizes of the figma design -->
+    <div class="h-1/5 bg-blue-300 flex items-center justify-center text-4xl text-gray-800">
+      header
     </div>
-    <div>Parent output: {{ text }}</div>
+    <div
+      class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex items-center justify-center text-4xl text-gray-800"
+    >
+      content
+    </div>
+    <div class="flex-grow bg-red-300 flex items-center justify-center text-4xl text-gray-800">
+      footer
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import { user, logout } from '../compositions/useAuthentication';
-import Textfield from '../components/uiBlocks/Textfield.vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  components: { Textfield },
   setup() {
-    const text = ref('');
-    return {
-      user,
-      logout,
-      text,
-    };
+    return {};
   },
 });
 </script>
