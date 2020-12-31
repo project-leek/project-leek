e57b4abcd13aa1fca23f2daea03c782cacc55106
Julian Hahn <julianhahn01@gmail.com>
2020-12-31 01:15:13 +0100

    better discribing html tags used

--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="wrapper" class="mx-auto max-w-lg h-full w-full flex flex-col">
     <!-- this is a basic template to fill the divs with content. The divs already match roughly the sizes of the figma design -->
-    <div class="h-1/5 bg-blue-300 flex items-center justify-center text-4xl text-gray-800">
+    <header class="h-1/5 bg-blue-300 flex items-center justify-center text-4xl text-gray-800">
       header
-    </div>
-    <div
+    </header>
+    <main
       class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex items-center justify-center text-4xl text-gray-800"
     >
       content
-    </div>
-    <div class="flex-grow bg-red-300 flex items-center justify-center text-4xl text-gray-800">
+    </main>
+    <footer class="flex-grow bg-red-300 flex items-center justify-center text-4xl text-gray-800">
       footer
-    </div>
+    </footer>
   </div>
 </template>
 
