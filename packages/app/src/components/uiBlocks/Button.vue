32a40d225cb50fa13217df8de506f8bf3d98fde4
Nick Loewecke <konten@loewecke.de>
2021-01-02 22:37:06 +0100

    make the button resizable

--- packages/app/src/components/uiBlocks/Button.vue
+++ packages/app/src/components/uiBlocks/Button.vue
@@ -1,18 +1,22 @@
 <template>
-  <button
-    class="bg-button border-2 border-button cursor-pointer text-center px-3 py-10 text-white shadow-xl rounded-full flex items-center"
-    @click="doClick"
-  >
-    <span class="flex px-2">
-      <span v-if="icon" class="mr-4 text-xl my-auto" :class="['fas', `fa-${icon}`]" />
-      <img v-if="imagePath" class="w-10 h-10 mr-2 br" :src="imagePath" />
-      <p class="my-auto font-heading text-3xl font-extralight">{{ text }}</p>
-    </span>
-  </button>
+  <div id="wraper">
+    <button
+      class="bg-button border-2 border-button cursor-pointer text-center px-3 py-1 text-white shadow-xl rounded-full flex items-center"
+      @click="doClick"
+    >
+      <span class="flex px-2">
+        <span v-if="icon" class="mr-4 text-xl my-auto" :class="['fas', `fa-${icon}`]" />
+        <img v-if="imagePath" class="w-10 h-10 mr-2 br" :src="imagePath" />
+        <p :class="['my-auto', 'font-heading', 'font-extralight', `text-${textsize}`]">
+          {{ text }}
+        </p>
+      </span>
+    </button>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 export default defineComponent({
@@ -42,6 +46,11 @@ export default defineComponent({
       required: false,
       default: '',
     },
+    size: {
+      type: Number,
+      required: false,
+      default: 3,
+    },
   },
   emits: ['click'],
   setup(props) {
@@ -62,8 +71,20 @@ export default defineComponent({
       }
     };
 
+    const textsize = ref('');
+    const one = 1;
+    console.log(props.size);
+    if (props.size === one) {
+      console.log('Eins');
+      textsize.value = 'xl';
+    } else if (props.size > 1) {
+      const tmpSize = props.size > 9 ? 9 : props.size;
+      textsize.value = `${tmpSize}xl`;
+    }
+
     return {
       doClick,
+      textsize,
     };
   },
 });
