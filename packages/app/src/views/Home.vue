065ab858e162bcdcbf9574b840d7c3ffa1be7da8
Julian Hahn <julianhahn01@gmail.com>
2021-01-02 23:42:09 +0100

    configured the add button with the new size configuration

--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="wrapper" class="mx-auto max-w-lg h-full w-full flex flex-col">
-    <header class="h-1/5 bg-blue-300 flex items-center justify-center text-4xl text-gray-800">
-      <Button icon="plus-square" text="Hinzufügen" size="1" />
+    <header class="h-1/5 flex items-end justify-center text-4xl text-gray-800">
+      <Button
+        icon="fas fa-plus-square"
+        text="Hinzufügen"
+        :textSize="2"
+        class="w-10/12 py-2 pl-3 mb-6 justify-start"
+      />
     </header>
     <main
       class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex items-center justify-center text-4xl text-gray-800"
