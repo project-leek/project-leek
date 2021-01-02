eb8d8e764486046d8cd803ec7f76366618f16859
Nick Loewecke <konten@loewecke.de>
2021-01-02 21:55:11 +0100

    Add "Add"-Button to Home

--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="wrapper" class="mx-auto max-w-lg h-full w-full flex flex-col">
     <header class="h-1/5 bg-blue-300 flex items-center justify-center text-4xl text-gray-800">
-      header
+      <Button class="mx-10 max-h-2 w-full" icon="plus-square" text="Hinzufügen" />
     </header>
     <main
       class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex items-center justify-center text-4xl text-gray-800"
@@ -16,9 +16,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Button from '../components/uiBlocks/Button.vue';
 
 export default defineComponent({
   name: 'Home',
+  components: {
+    Button,
+  },
   setup() {
     return {};
   },
