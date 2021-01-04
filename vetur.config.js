7ae6ac0932457b217d8f3fa1045edc5efb9a9cdb
Anton Bracke <anton@ju60.de>
2021-01-04 09:41:32 +0100

    improve vetur config

--- vetur.config.js
+++ vetur.config.js
@@ -1,5 +1,17 @@
-// vetur.config.js
 /** @type {import('vls').VeturConfig} */
 module.exports = {
-  projects: ['./packages/app'],
+  // **optional** default: `{}`
+  // override vscode settings
+  // Notice: It only affects the settings used by Vetur.
+  settings: {
+    'vetur.useWorkspaceDependencies': true,
+    'vetur.experimental.templateInterpolationService': true,
+    'vetur.completion.tagCasing': 'initial',
+    'vetur.validation.templateProps': true,
+  },
+  // **optional** default: `[{ root: './' }]`
+  // support monorepos
+  projects: [
+    './packages/app', // shorthand for only root.
+  ],
 };
