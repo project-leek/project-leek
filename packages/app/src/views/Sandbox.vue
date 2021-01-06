582213d3525ef9a84cdd4f5743fbae5957cc9c5c
Nick Loewecke <konten@loewecke.de>
2021-01-06 23:10:42 +0100

    Add Sandbox to see all uiBlocks &  necessary props

---
+++ packages/app/src/views/Sandbox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="w-full h-full">
+    <h1 class="text-3xl text-center bg-primary py-2">Sandbox</h1>
+    <div class="p-2">
+      <!-- Dropdown -->
+      <h1 class="text-2xl text-center bg-secondary py-1">Dropdown</h1>
+      <Dropdown
+        v-model="selValue"
+        class="mb-5 h-20"
+        service="nfc-tags"
+        value-property="nfcId"
+        label="NFC-Tag"
+        add-item-option
+      />
+      <!-- Button -->
+      <h1 class="text-2xl text-center bg-secondary py-1">Buttons</h1>
+      <Button class="w-full h-15 my-5" text="Drück mich!" />
+      <Button class="w-full h-15 my-5" icon="fas fa-sun" text="Drück mich!" />
+      <Button class="w-full h-15 my-5" icon="fas fa-sun" />
+      <Button class="h-20 w-20 my-5" icon="fas fa-sun" round />
+
+      <!-- Textfield -->
+      <h1 class="text-2xl text-center bg-secondary py-1">Textfield</h1>
+      <Textfield class="w-full h-15 my-5" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+import Button from '../components/uiBlocks/Button.vue';
+import Dropdown, { ListItem } from '../components/uiBlocks/Dropdown.vue';
+import Textfield from '../components/uiBlocks/Textfield.vue';
+
+export default defineComponent({
+  name: 'SandBox',
+  components: {
+    Button,
+    Dropdown,
+    Textfield,
+  },
+  setup() {
+    const dropDownValue = ref(new ListItem());
+
+    return {
+      dropDownValue,
+    };
+  },
+});
+</script>
