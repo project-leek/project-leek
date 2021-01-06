de2f9bdc1175b7f34a3aba2b310e89c6d9b2e67a
Julian Hahn <julianhahn01@gmail.com>
2021-01-06 20:50:46 +0100

    quicksafe

---
+++ packages/app/src/components/uiBlocks/TagEntry.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="w-20 bg-red-400">
+    <img class=" w-full h-20  object-scale-down" :src="image" />
+    <div id="TagName" class="text-2xl text-white font-semibold">{{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    img: {
+      type: String,
+      required: true,
+      default: '',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const image = `./${props.img}`;
+    return { image };
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="wrapper" class="mx-auto max-w-lg h-full w-full flex flex-col overflow-hidden">
-    <header class="h-1/5 bg-blue-300 flex items-center justify-center text-4xl text-gray-800">
-      header
-    </header>
+    <header class="h-1/5 flex items-center justify-center text-4xl text-gray-800">header</header>
     <main
-      class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex items-center justify-center text-4xl text-gray-800"
+      class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex flex-col text-4xl text-gray-800"
     >
-      {{ searchInput }}
+      <div class="w-full bg-blue-900 h-64">
+        <div class="text-white text-2xl pl-10 pb-2">Zuletzt gehört</div>
+        <tag-entry class="bg-opacity-20 bg-white w-32" name="Dynamite" img="logo.png" />
+      </div>
     </main>
     <footer class="flex-grow flex items-center justify-center text-4xl text-gray-800">
       <Textfield v-model="searchInput" class="w-5/6" placeholder="Titelsuche" icon="search" />
@@ -17,11 +18,13 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import Textfield from '../components/uiBlocks/Textfield.vue';
+import TagEntry from '../components/uiBlocks/TagEntry.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     Textfield,
+    TagEntry,
   },
   setup() {
     const searchInput = ref('');
