975110769644bf84438c477a6011976f07e31d81
Anton Bracke <anton@ju60.de>
2021-01-11 20:29:34 +0100

    add addTag view and route

---
+++ packages/app/src/views/AddTag.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="add-tag">
+    <pre>{{ nfcTag }}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import { NFCTag } from '@leek/commons';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'AddTag',
+
+  setup() {
+    const nfcTag = new NFCTag();
+
+    return {
+      nfcTag,
+    };
+  },
+});
+</script>
+
+<style lang="css">
+.app {
+  height: 100vh;
+}
+</style>
