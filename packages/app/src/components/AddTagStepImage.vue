3e6cf98fe736cde74d9b267b23a8da9aa7578050
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2021-01-11 15:56:45 +0100

    added layout

--- packages/app/src/components/AddTagStepImage.vue
+++ packages/app/src/components/AddTagStepImage.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div class="flex flex-grow flex-col justify-start items-start">
+    <div class="m-8 text-lg font-semibold text-white">Bild von Spotify</div>
+    <div class="ml-8">Tag Entry</div>
+  </div>
 </template>
 
 <script lang="ts">
