4c2b151a11d21989c12fb9cc6818f80705d8e0b2
Anton Bracke <anton@ju60.de>
2021-01-11 21:28:25 +0100

    update vite

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,11 +1,19 @@
-export default {
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3030',
-      ws: true,
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [vue()],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3030',
+        ws: true,
+      },
     },
   },
+
   optimizeDeps: {
     exclude: ['@fortawesome/fontawesome-free'],
   },
-};
+});
