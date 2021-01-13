e8379c1d8f598cef0ebee2ab4e9bb48c0283af2f
Anton Bracke <anton@ju60.de>
2021-01-13 19:05:15 +0100

    add tag image step

--- packages/app/src/components/add-tag/AddTagStepImage.vue
+++ packages/app/src/components/add-tag/AddTagStepImage.vue
@@ -1,27 +1,29 @@
 <template>
   <div class="flex flex-grow flex-col justify-start items-start">
     <div class="m-8 text-lg font-semibold text-white">Bild von Spotify</div>
-    <TagEntry class="ml-8 w-32" :img="mImageURL" />
+    <TagEntry class="ml-8 w-32" :img="imageUrl" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
-import TagEntry from './uiBlocks/TagEntry.vue';
+import TagEntry from '../uiBlocks/TagEntry.vue';
 
 export default defineComponent({
   name: 'AddTagStepImage',
+
   components: { TagEntry },
+
   props: {
-    imageURL: {
+    imageUrl: {
       type: String,
       required: true,
     },
   },
-  setup(props) {
-    const mImageURL = ref(props.imageURL);
-    return { mImageURL };
+
+  setup() {
+    return {};
   },
 });
 </script>
