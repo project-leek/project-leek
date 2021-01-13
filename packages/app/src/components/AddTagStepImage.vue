45ffa0638f8c60a47b7c293ebe3ac93310108a11
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2021-01-13 10:42:48 +0100

    added imageprop

--- packages/app/src/components/AddTagStepImage.vue
+++ packages/app/src/components/AddTagStepImage.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="flex flex-grow flex-col justify-start items-start">
     <div class="m-8 text-lg font-semibold text-white">Bild von Spotify</div>
-    <TagEntry class="ml-8 w-32" name="" img="{{mImage}}" />
+    <TagEntry class="ml-8 w-32" :img="mImageURL" />
   </div>
 </template>
 
 <script lang="ts">
-import { NFCTag } from '@leek/commons/';
-import { defineComponent, PropType, ref } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 import TagEntry from './uiBlocks/TagEntry.vue';
 
@@ -15,20 +14,14 @@ export default defineComponent({
   name: 'AddTagStepImage',
   components: { TagEntry },
   props: {
-    nfcTag: {
-      type: Object as PropType<NFCTag>,
-      required: true,
-    },
-    image: {
+    imageURL: {
       type: String,
       required: true,
-      default: '',
     },
   },
   setup(props) {
-    const mImage = ref<string>(props.image);
-    const mNFCTag = ref(props.nfcTag);
-    return { mImage, mNFCTag };
+    const mImageURL = ref(props.imageURL);
+    return { mImageURL };
   },
 });
 </script>
