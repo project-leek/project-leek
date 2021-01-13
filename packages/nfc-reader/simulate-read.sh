154ce0c963086fabb1c409fe80e90cfa3b728418
Anton Bracke <anton@ju60.de>
2021-01-13 17:25:09 +0100

    add script to simulate tag attaching

---
+++ packages/nfc-reader/simulate-read.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+NFC_READER_ID="BU62UGHufeLvF763"
+NFC_TAG_DATA="2589689541"
+API_URL="http://localhost:3030"
+# API_URL="http://192.168.0.182:3030"
+
+curl -H "Content-Type: application/json" -X PATCH -d '{"attachedTagData": "'$NFC_TAG_DATA'"}' "$API_URL/nfc-readers/$NFC_READER_ID"
+sleep 3
+curl -H "Content-Type: application/json" -X PATCH -d '{"attachedTagData": null}' "$API_URL/nfc-readers/$NFC_READER_ID"
