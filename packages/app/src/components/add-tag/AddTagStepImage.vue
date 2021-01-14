88320bb592b842298e7ac5e48e0d3afa423c6d0d
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2021-01-14 09:04:48 +0100

    ability to add an image from the web

--- packages/app/src/components/add-tag/AddTagStepImage.vue
+++ packages/app/src/components/add-tag/AddTagStepImage.vue
@@ -1,30 +1,36 @@
 <template>
   <div class="add-tag-step-image flex flex-grow flex-col justify-start items-start">
     <div class="m-8 text-lg font-semibold text-white">Bild von Spotify</div>
-    <TagEntry class="ml-8 w-32" :img="nfcTag.imageUrl" />
+    <TagEntry class="ml-8 w-32" img="nfcTag.imageUrl-:vorneran" />
+    <div class="m-8 text-lg font-semibold text-white">Bild aus dem Internet</div>
+    <TagEntry class="ml-8 w-32" :img="externalImage" />
+    <Textfield v-model="externalImage" class="m-8 w-full" placeholder="enter URL" />
   </div>
 </template>
 
 <script lang="ts">
 import { NFCTag } from '@leek/commons';
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 
 import TagEntry from '../uiBlocks/TagEntry.vue';
+import Textfield from '../uiBlocks/Textfield.vue';
 
 export default defineComponent({
   name: 'AddTagStepImage',
 
-  components: { TagEntry },
+  components: { TagEntry, Textfield },
 
   props: {
     nfcTag: {
       type: Object as PropType<NFCTag>,
-      required: true,
+      required: false, //set to true after testing
+      default: null,
     },
   },
 
   setup() {
-    return {};
+    const externalImage = ref<string>('');
+    return { externalImage };
   },
 });
 </script>
--- packages/app/src/views/ChangeImage.vue
+++ packages/app/src/views/ChangeImage.vue
@@ -1,61 +1,27 @@
 <template>
-  <div id="wrapper"
-class="mx-auto max-w-lg h-full w-full flex flex-col">
-    <header class="h-1/5 flex items-end justify-center text-4xl text-gray-800">
-      <div>{{ mTagName }}</div>
-      <div>Registrierte Tag-ID #{{ mTagID }}</div>
-    </header>
+  <div id="wrapper" class="mx-auto max-w-lg h-full w-full flex flex-col">
+    <header class="h-1/5 flex items-end justify-center text-4xl text-gray-800"></header>
     <main
       class="h-2/3 overflow-x-hidden overflow-y-scroll bg-yellow-300 flex items-center justify-center text-4xl text-gray-800"
     >
-      <div>
-        <div>
-          <div>Bild von Spotify</div>
-          <div @click="useSpotify">INSERT SPOTIFY IMAGE</div>
-        </div>
-
-        <div>
-          <div>Mit eigenem Bild ersetzen</div>
-          <div @click="useExplorer">+</div>
-        </div>
-      </div>
+      <AddTagStepImage />
     </main>
     <footer class="flex-grow flex items-center justify-center text-4xl text-gray-800" />
-    <Button icon="fas fa-caret-left"
-round />
-    <Button round
-:text="Speichern" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 
-import Button from '../components/uiBlocks/Button.vue';
-import Textfield from '../components/uiBlocks/Textfield.vue';
+import AddTagStepImage from '../components/add-tag/AddTagStepImage.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    Button,
-    Textfield,
+    AddTagStepImage,
   },
-  props: {
-    tagName: {
-      type: String,
-      required: false, //change to true
-      default: 'Geschichten aus der Geschichte',
-    },
-    tagID: {
-      type: Number,
-      required: false, //change to true
-      default: 243762,
-    },
-  },
-  setup(props) {
-    const mTagName = ref(props.tagName);
-    const mTagID = ref(props.tagID);
-
+  props: {},
+  setup() {
     const useExplorer = () => {
       return; //todo implement opening explorer
     };
@@ -64,7 +30,7 @@ export default defineComponent({
       return; //todo implement showing green border around spotify image
     };
 
-    return { mTagName, mTagID, useSpotify, useExplorer };
+    return {};
   },
 });
 </script>
