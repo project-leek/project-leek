9975dd405855f2eda2fd1b40645004f68967be88
Anton Bracke <anton@ju60.de>
2021-01-15 09:49:44 +0100

    remove label, add placeholder color to dropdown

--- packages/app/src/components/uiBlocks/Dropdown.vue
+++ packages/app/src/components/uiBlocks/Dropdown.vue
@@ -1,54 +1,52 @@
 <template>
-  <div class="relative h-10 min-w-min">
-    <span v-if="label" class="ml-6 text-base text-white font-heading">{{ label }}</span>
+  <div
+    class="bg-white p-1 pr-3 rounded-3xl w-full flex flex-col text-lg border-button shadow-lg border-2 outline-none focus:ring-2 focus:ring-yellow-400 focus:ring-opacity-25"
+    :class="{ 'z-50': dropdownExtended }"
+  >
     <div
-      class="bg-white p-1 pr-3 rounded-3xl w-full flex flex-col text-lg border-button shadow-lg border-2 outline-none"
-      :class="{ 'z-50': dropdownExtended }"
+      class="flex items-center pl-2 cursor-pointer"
+      @click="dropdownExtended = !dropdownExtended"
     >
-      <div
-        class="flex items-center pl-2 cursor-pointer"
-        @click="dropdownExtended = !dropdownExtended"
-      >
-        <span class="label">{{ (modelValue && modelValue.value) || placeholderText }}</span>
-        <Button
-          v-if="dropdownExtended"
-          class="ml-auto w-8 h-8"
-          icon="fas fa-chevron-down"
-          :icon-size="3"
-          round
-        />
-        <Button v-else class="ml-auto w-8 h-8" icon="fas fa-chevron-right" :icon-size="3" round />
-      </div>
-      <div v-if="dropdownExtended" class="divide-y divide-yellow-50 static z-50">
-        <div v-for="item in items" :key="item.id">
-          <hr class="w-full border-dotted border-secondary border-1 my-2" />
-          <div
-            :class="{ 'bg-yellow-400': modelValue && modelValue.id === item.id }"
-            class="item flex w-full cursor-pointer hover:bg-yellow-200 rounded-2xl"
-            @click="selectItem(item)"
-          >
-            <span class="px-2">{{ item.value }}</span>
-            <Button
-              v-if="removeable"
-              class="w-8 h-8 ml-auto"
-              icon="far fa-trash-alt"
-              :icon-size="3"
-              round
-              @click="removeItem(item)"
-            />
-          </div>
+      <span v-if="modelValue && modelValue.value" class="value">{{ modelValue.value }}</span>
+      <span v-else class="placeholder text-gray-400">{{ placeholderText }}</span>
+      <Button
+        v-if="dropdownExtended"
+        class="ml-auto w-8 h-8"
+        icon="fas fa-chevron-down"
+        :icon-size="3"
+        round
+      />
+      <Button v-else class="ml-auto w-8 h-8" icon="fas fa-chevron-right" :icon-size="3" round />
+    </div>
+    <div v-if="dropdownExtended" class="divide-y divide-yellow-50 static z-50">
+      <div v-for="item in items" :key="item.id">
+        <hr class="w-full border-dotted border-secondary border-1 my-2" />
+        <div
+          :class="{ 'bg-yellow-400': modelValue && modelValue.id === item.id }"
+          class="item flex w-full cursor-pointer hover:bg-yellow-200 rounded-2xl"
+          @click="selectItem(item)"
+        >
+          <span class="px-2">{{ item.value }}</span>
+          <Button
+            v-if="removeable"
+            class="w-8 h-8 ml-auto"
+            icon="far fa-trash-alt"
+            :icon-size="3"
+            round
+            @click="removeItem(item)"
+          />
         </div>
-        <div v-if="enableAddItem" class="add-item">
-          <hr class="w-full border-solid border-secondary border-1 my-2" />
-          <div class="add-item-section flex w-full items-center pt-0.5">
-            <input
-              v-model="newItemValue"
-              :placeholder="addItemText"
-              class="flex-grow p-2 rounded-2xl focus:outline-none"
-              @keyup.enter="addItem"
-            />
-            <Button class="w-8 h-8" icon="fas fa-plus" :icon-size="3" round @click="addItem" />
-          </div>
+      </div>
+      <div v-if="enableAddItem" class="add-item">
+        <hr class="w-full border-solid border-secondary border-1 my-2" />
+        <div class="add-item-section flex w-full items-center pt-0.5">
+          <input
+            v-model="newItemValue"
+            :placeholder="addItemText"
+            class="flex-grow p-2 rounded-2xl focus:outline-none"
+            @keyup.enter="addItem"
+          />
+          <Button class="w-8 h-8" icon="fas fa-plus" :icon-size="3" round @click="addItem" />
         </div>
       </div>
     </div>
@@ -90,11 +88,6 @@ export default defineComponent({
       type: String,
       required: false,
       default: 'Please add an element',
-    },
-    label: {
-      type: String,
-      required: false,
-      default: '',
     },
     items: {
       type: Array as PropType<ListItem[]>,
