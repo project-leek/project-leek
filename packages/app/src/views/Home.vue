9d1fbbb6f924bc656f483547c217b029b8c261c5
Julian Hahn <julianhahn01@gmail.com>
2021-01-20 12:43:07 +0100

    quicksafe of first idea of a blurred background

--- packages/app/src/views/Home.vue
+++ packages/app/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home w-full flex flex-col">
-    <header class="flex my-5 items-end justify-center text-4xl text-gray-800">
+    <header class="flex py-5 justify-center text-4xl overlay" :class="{ blurred: selectedTag }">
       <Button
         :to="{ name: 'add-tag' }"
         icon="fas fa-plus-square"
@@ -22,15 +22,15 @@
             <TagEntry
               v-for="entry in group.tags"
               :key="entry.nfcData"
-              class="m-4 w-2/6 flex-shrink-0 text-4xl text-gray-800"
+              class="m-4 w-2/6 flex-shrink-0 text-4xl"
               :img="entry.imageUrl"
               :name="entry.name"
+              @click="selectedTag = entry"
             />
           </div>
         </GroupDropDownItem>
       </GroupDropDown>
     </main>
-
     <footer class="flex text-gray-800 py-5">
       <span class="text-xl text-center w-full"
         >&copy; {{ new Date().getFullYear() }} - Made with ❤️ by
@@ -70,6 +70,7 @@ export default defineComponent({
 
   setup() {
     const groups = ref<NFCTagGroup[]>([]);
+    const selectedTag = ref<NFCTag | null>(null);
 
     onMounted(async () => {
       const res = (await feathers.service('nfc-tags').find()) as Paginated<NFCTag>;
@@ -85,7 +86,23 @@ export default defineComponent({
       );
     });
 
-    return { groups };
+    return { groups, selectedTag };
   },
 });
 </script>
+
+<style lang="css" scoped>
+.overlay {
+  position: relative;
+}
+
+.overlay.blurred::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  backdrop-filter: blur(2px);
+}
+</style>
