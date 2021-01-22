c26c08d81324441e6b216dec40d2cfde9fcf2fea
Nick Loewecke <konten@loewecke.de>
2021-01-22 17:52:14 +0100

    Add Logout Button and User-Mail

--- packages/app/src/views/Settings.vue
+++ packages/app/src/views/Settings.vue
@@ -4,16 +4,27 @@
       <Button back round icon="fas fa-times" class="h-7 w-7" />
       <span class="ml-2 text-3xl">Einstellungen</span>
     </header>
-    <main class="bg-secondary max-h-full overflow-y-auto flex-grow p-6">
-      <LabeledInput class="mb-5 h-20" label="Lautsprecher">
+    <main class="bg-secondary max-h-full overflow-y-auto flex-grow p-6 text-white">
+      <!-- Useranzeige -->
+      <div class="flex flex-col justify-center items-center">
+        <span class="flex text-xl font-heading">
+          <span class="my-auto fab fa-spotify" />
+          <span class="ml-2">Nutzer:</span>
+        </span>
+        <span class="text-2xl font-semibold">{{ userEmail }}</span>
+      </div>
+
+      <!-- Lautsprecherauswahl -->
+      <LabeledInput class="mb-5 h-20 mt-10" label="Lautsprecher">
         <Dropdown v-model="selectedSpeaker" :items="speakerList" :removeable="false" />
       </LabeledInput>
     </main>
 
     <footer class="flex text-gray-800 py-5">
       <span class="flex w-full text-xl">
-        <Button round icon="fas fa-caret-left" class="ml-4 my-auto h-8 w-8" />
-        <Button text="Speichern" center-text class="mx-4 px-3 text-2xl flex-grow" />
+        <Button round icon="fas fa-caret-left" class="ml-4 my-auto h-12 w-12" />
+        <Button text="Speichern" center-text class="mx-2 px-3 py-2 text-2xl flex-grow" />
+        <Button text="Abmelden" center-text class="mx-2 px-3 text-2xl flex-grow" />
       </span>
     </footer>
   </div>
@@ -27,30 +38,45 @@ import Button from '../components/uiBlocks/Button.vue';
 import ListItem from '../components/uiBlocks/Dropdown.ListItem';
 import Dropdown from '../components/uiBlocks/Dropdown.vue';
 import LabeledInput from '../components/uiBlocks/LabeledInput.vue';
+import { isAuthenticated, user } from '../compositions/useAuthentication';
 import feathers from '../lib/feathers';
 
 export default defineComponent({
   name: 'Settings',
   components: { Button, Dropdown, LabeledInput },
   setup() {
+    const userEmail = ref<string>();
     const selectedSpeaker = ref<SpotifySpeaker>();
     const speakers = ref<SpotifySpeaker[]>([]);
+
     const speakerList = computed(() => {
       return speakers.value.map((s) => {
         return new ListItem(s._id, s.name);
       });
     });
 
-    onMounted(async () => {
+    const loadSpeakers = async (): Promise<void> => {
       const allSpeakers = (await feathers.service('spotify-speakers').find()) as SpotifySpeaker[];
       if (allSpeakers) {
         speakers.value = allSpeakers;
       }
+    };
+
+    const loadUser = (): void => {
+      if (isAuthenticated) {
+        userEmail.value = user.value?.email;
+      }
+    };
+
+    onMounted(async () => {
+      await loadSpeakers();
+      loadUser();
     });
 
     return {
       selectedSpeaker,
       speakerList,
+      userEmail,
     };
   },
 });
