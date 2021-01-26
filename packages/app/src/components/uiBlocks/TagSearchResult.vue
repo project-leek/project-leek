10b0f56e0e6e0b32bd0b7e87a25e3de23117dff1
JanEberlein <eberlein.jan@aol.com>
2021-01-26 11:15:57 +0100

    - adds component for showing tag search result for home screen

---
+++ packages/app/src/components/uiBlocks/TagSearchResult.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="grid grid-cols-2">
+    <TagEntry
+      v-for="tag in searchResult"
+      :key="tag.nfcData"
+      class="m-4 w-2/6 flex-shrink-0 text-4xl"
+      :class="{ 'opacity-25': selectedTag !== tag && selectedTag !== null }"
+      :img="tag.imageUrl"
+      :name="tag.name"
+      @click="$emit('update:selected-tag', tag)"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Paginated } from '@feathersjs/feathers';
+import { NFCTag } from '@leek/commons';
+import { defineComponent, onMounted, PropType, ref } from 'vue';
+
+import feathers from '../../lib/feathers';
+import TagEntry from './TagEntry.vue';
+
+export default defineComponent({
+  name: 'TagSearchResult',
+
+  components: {
+    TagEntry,
+  },
+
+  props: {
+    selectedTag: {
+      type: Object as PropType<NFCTag | null>,
+      default: null,
+    },
+  },
+
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    'update:selected-tag': (_payload: NFCTag | null): boolean => true,
+  },
+
+  setup() {
+    const searchResult = ref<NFCTag[]>([]);
+
+    async function loadTags(): Promise<void> {
+      const service = feathers.service('nfc-tags');
+      const allTags = (await service.find()) as Paginated<NFCTag>;
+      searchResult.value = allTags.data;
+    }
+
+    onMounted(async () => await loadTags());
+
+    return {
+      searchResult,
+    };
+  },
+});
+</script>
