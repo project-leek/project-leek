3118eb6731774bb73651b16931371df9a2ffd958
Anton Bracke <anton@ju60.de>
2021-01-27 11:00:50 +0100

    remove seeds

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -7,61 +7,6 @@ import webserver from './webserver';
 const NODE_ENV = process.env.NODE_ENV || 'production';
 let server: Server;
 
-async function seed(): Promise<void> {
-  await app.service('nfc-readers').create({
-    _id: 'BU62UGHufeLvF763', // use a fixed id to prevent duplicates
-    owner: 'OGPy2N1spnM2Ov50',
-    attachedTagData: null,
-  });
-}
-
-async function seedTags(): Promise<void> {
-  await app.service('nfc-tags').create({
-    _id: 'asdfsadfsafd', // use a fixed id to prevent duplicates
-    spotifyTrackUri: 'https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa',
-    nfcData: 'flonke1',
-    imageUrl: 'https://cdn.pixabay.com/photo/2020/12/17/14/07/leaves-5839550_960_720.jpg',
-    name: 'erster Eintrag',
-    group: 'Lieblingstracks',
-  });
-
-  await app.service('nfc-tags').create({
-    _id: '123jgh123', // use a fixed id to prevent duplicates
-    spotifyTrackUri: 'https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa',
-    imageUrl: 'https://cdn.pixabay.com/photo/2013/08/20/15/47/poppies-174276_960_720.jpg',
-    name: ' zweiter Name',
-    group: 'Lieblingstracks',
-    nfcData: 'flonke2',
-  });
-
-  await app.service('nfc-tags').create({
-    _id: '1231jh2g31jh3jhd', // use a fixed id to prevent duplicates
-    spotifyTrackUri: 'https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa',
-    imageUrl: 'https://cdn.pixabay.com/photo/2013/07/21/13/00/rose-165819_960_720.jpg',
-    name: 'name aus dem Tag',
-    group: 'Entdecken',
-    nfcData: 'flonke3',
-  });
-
-  await app.service('nfc-tags').create({
-    _id: '123babjajdf', // use a fixed id to prevent duplicates
-    spotifyTrackUri: 'https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa',
-    imageUrl: 'https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313_960_720.jpg',
-    group: 'Entdecken',
-    name: 'good boy',
-    nfcData: 'flonke4',
-  });
-
-  await app.service('nfc-tags').create({
-    _id: '123babj123123ajdf', // use a fixed id to prevent duplicates
-    spotifyTrackUri: 'https://api.spotify.com/v1/tracks/2KrxsD86ARO5beq7Q0Drfqa',
-    imageUrl: 'https://cdn.pixabay.com/photo/2020/12/20/04/06/bear-5846065_960_720.jpg',
-    group: 'Entdecken',
-    name: 'Bär in Kiel',
-    nfcData: 'flonke5',
-  });
-}
-
 function start(): void {
   logger.info('Application (%s v%s) starting ...', NODE_ENV, 'unkown');
 
@@ -74,16 +19,6 @@ function start(): void {
   });
 
   app.setup(server);
-
-  seedTags().catch((error: Error) => {
-    // eslint-disable-next-line no-console
-    console.error('Seeding tags failed', error.message);
-  });
-
-  seed().catch((error: Error) => {
-    // eslint-disable-next-line no-console
-    console.error('Seeding failed', error.message);
-  });
 }
 
 async function stop(): Promise<void> {
