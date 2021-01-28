d8bb7f15215688b186312d6e0ed73394e46ed714
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2021-01-28 07:29:54 +0100

    better feedback which image is chosen

--- packages/app/src/components/add-tag/AddTagStepImage.vue
+++ packages/app/src/components/add-tag/AddTagStepImage.vue
@@ -2,18 +2,22 @@
   <div class="add-tag-step-image flex flex-grow flex-col justify-start items-start px-8 py-5">
     <div class="text-lg pb-1 font-semibold text-white">Bild von Spotify</div>
     <div class="flex content-end">
-      <TagEntry class="w-32" :img="spotifyImageUrl" @click="changeImage(true)" />
-      <span
-        v-if="usingSpotifyImage"
-        class="text-white text-opacity-60 text-4xl far fa-check-circle transform -translate-x-20 translate-y-10"
+      <TagEntry
+        class="w-32"
+        :img="spotifyImageUrl"
+        :ring="usingSpotifyImage"
+        ring-color="ring-green-800"
+        @click="changeImage(true)"
       />
     </div>
     <div class="pt-8 pb-1 text-lg font-semibold text-white">Bild aus dem Internet</div>
     <div class="flex content-end">
-      <TagEntry class="w-32" :img="externalImage" @click="changeImage(false)" />
-      <span
-        v-if="!usingSpotifyImage"
-        class="text-white text-opacity-60 text-4xl far fa-check-circle transform -translate-x-20 translate-y-10"
+      <TagEntry
+        class="w-32"
+        :img="externalImage"
+        :ring="!usingSpotifyImage"
+        ring-color="ring-green-800"
+        @click="changeImage(false)"
       />
     </div>
     <Textfield v-model="userInput" class="mt-2 w-full" placeholder="enter URL" />
