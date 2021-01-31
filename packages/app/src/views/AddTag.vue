4bb07e65a319c61017c88966969da17db7855a4a
Tom Calvin Haak <9517685+tcoro@users.noreply.github.com>
2021-01-31 16:01:46 +0100

    made tag id less prominent info

--- packages/app/src/views/AddTag.vue
+++ packages/app/src/views/AddTag.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="add-tag w-full flex flex-col">
     <header class="px-8 h-24 flex flex-row items-center">
-      <Button back round icon="fas fa-times" class="h-10 w-10" :icon-size="6" />
-      <div class="headlines ml-2 flex flex-col my-auto">
+      <Button back round icon="fas fa-times" class="h-14 w-14" :icon-size="6" />
+      <div class="headlines ml-5 flex flex-col my-auto">
         <span class="text-3xl">Neuen Tag anlegen</span>
-        <span v-if="nfcTag && nfcTag.nfcData">Tag-ID: #{{ nfcTag.nfcData }}</span>
+        <span v-if="nfcTag && nfcTag.nfcData" class="italic font-thin text-black text-opacity-30"
+          >Tag-ID: #{{ nfcTag.nfcData }}</span
+        >
       </div>
     </header>
 
