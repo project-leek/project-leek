0e88a3ca4e7a44b4583a3f85e0a86e4ff11cde7b
Nick Loewecke <Nick.Loewecke@student.fh-kiel.de>
2021-02-03 13:00:45 +0100

    removing tag-result

--- packages/commons/src/index.ts
+++ packages/commons/src/index.ts
@@ -2,8 +2,7 @@ import { AbstractEntity, Ref } from './abstract-entity.class';
 import NFCReader from './nfc-reader.class';
 import NFCTag from './nfc-tag.class';
 import Speaker from './speaker.class';
-import TagResult from './tag-result.class';
 import Track from './track.class';
 import User from './user.class';
 
-export { AbstractEntity, NFCReader, NFCTag, Ref, Speaker, TagResult, Track, User };
+export { AbstractEntity, NFCReader, NFCTag, Ref, Speaker, Track, User };
--- packages/commons/src/tag-result.class.ts
+++
@@ -1,6 +0,0 @@
-import NFCTag from './nfc-tag.class';
-
-export default class TagResult {
-  tag!: NFCTag;
-  valid!: boolean;
-}
