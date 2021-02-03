9fb748f673310fa8cb9404fbf3f97639bca02c3c
Anton Bracke <anton@ju60.de>
2021-02-03 11:30:01 +0100

    improve background

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="custom-shape-divider-top-1611822391">
+  <div class="lg:flex hidden background">
     <svg
       data-name="Layer 1"
       xmlns="http://www.w3.org/2000/svg"
@@ -9,6 +9,7 @@
       <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
     </svg>
   </div>
+
   <div class="h-screen bg-yellow-600 flex flex-col items-center justify-center">
     <div
       class="z-50 flex lg:shadow-2xl lg:max-w-3xl lg:max-h-3/4 bg-white lg:rounded-3xl w-full h-full"
@@ -46,23 +47,22 @@ export default defineComponent({
 </script>
 
 <style lang="css">
-.custom-shape-divider-top-1611822391 {
+.background {
   position: absolute;
+  display: flex;
   top: 0;
   left: 0;
   width: 100%;
-  overflow: hidden;
-  line-height: 0;
+  height: 100%;
 }
 
-.custom-shape-divider-top-1611822391 svg {
-  position: relative;
+.background svg {
   display: block;
-  width: 140%;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 }
 
-.custom-shape-divider-top-1611822391 .shape-fill {
+.background .shape-fill {
   fill: #f09000;
 }
 </style>
