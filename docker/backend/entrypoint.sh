a1ff1348ee60f7e77175946fbd3df58625c0145a
Anton Bracke <anton@ju60.de>
2021-02-11 01:54:45 +0100

    add ngrok to backend

---
+++ docker/backend/entrypoint.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+# skip ngrok
+if [ -n "$BE_FREE" ]; then
+  node index.js
+  exit 0
+fi
+
+# use ngrok and use auto-generated hostname for backend
+if [ -z "$NGROK_AUTH" ]; then
+  echo "Please set NGROK_AUTH to your ngrok auth token."
+  echo "You can get it from: https://dashboard.ngrok.com/get-started/setup"
+  exit -1
+fi
+
+echo "Starting ngrok ..."
+/bin/ngrok http backend:3030 --authtoken $NGROK_AUTH --bind-tls true --log stdout --region eu &
+
+sleep .7 # Wait for ngrok launch
+
+export NGROK_HOSTNAME=$(echo -e 'GET /api/tunnels HTTP/1.1\nHost: 127.0.0.1\n' | nc localhost 4040 | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
+export HOSTNAME=$NGROK_HOSTNAME
+
+node index.js
